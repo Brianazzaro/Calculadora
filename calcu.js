e8e0925bc75e832f24e08783f7e9cc35aee96105
Brian <105122688+Brianazzaro@users.noreply.github.com>
2022-12-11 16:39:50 -0300

    Create calcu.js

---
+++ calcu.js
@@ -0,0 +1,24 @@
+
+var operaciones = function (e) {
+    var numero1 = parseFloat(document.getElementById("numero1").nodeValue);
+    var numero2 = parseFloat(document.getElementById("numero2").nodeValue);
+    var result = document.getElementById("resultado");
+    var operacion = e.target.id;
+    if (operacion == "suma") {
+        var resultado = numero1 + numero2;
+    } else if (operacion == "resta") {
+        var resultado = numero1 - numero2;
+
+    } else if (operacion == "multiplica") {
+        var resultado = numero1 * numero2;
+
+    } else if (operacion == "divide") {
+        var resultado = numero1 / numero2;
+
+    }
+    result.innerHTML = resultado;
+
+
+}
+var botones = document.getElementById("btns");
+botones.addEventListener("click", operaciones);
